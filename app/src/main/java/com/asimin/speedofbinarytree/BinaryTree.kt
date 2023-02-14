e9295536d82fb9373a1b3af35e93b9fbf6dd4fc4
Aleksandar Simin <simin@sbb.rs>
2023-02-14 20:01:54 +0100

    002 - Created BinaryTree.kt which have functionality of creation, getRandomNode, getNodeByIndex, clear BinaryTree - added functionality to GENERATE BT button which generate new Binary Tree of N nodes od Strings of 10 chars, then return random or by given index node value and index - added functionality to SEARCH button which search binary tree for given String - all operation are metered for execution time and result is written in related text view field - for generate binary tree I am used Kotlin coroutines - lifecycleScope.

---
+++ app/src/main/java/com/asimin/speedofbinarytree/BinaryTree.kt
@@ -0,0 +1,227 @@
+package com.asimin.speedofbinarytree
+
+import java.util.*
+
+class Node<T>(var value: T, var left: Node<T>? = null, var right: Node<T>? = null)
+
+class BinaryTree<T : Comparable<T>> {
+    var root: Node<T>? = null
+
+    fun addNode(value: T) {
+        root = addRecursive(root, value)
+    }
+
+    private fun addRecursive(current: Node<T>?, value: T): Node<T> {
+        if (current == null) {
+            return Node(value)
+        }
+
+        if (value < current.value) {
+            current.left = addRecursive(current.left, value)
+        } else if (value > current.value) {
+            current.right = addRecursive(current.right, value)
+        } else {
+            // value already exists
+            return current
+        }
+
+        return current
+    }
+
+    fun containsNode(value: T): Boolean {
+        return containsNodeRecursive(root, value)
+    }
+
+    private fun containsNodeRecursive(current: Node<T>?, value: T): Boolean {
+        if (current == null) {
+            return false
+        }
+
+        if (value == current.value) {
+            return true
+        }
+
+        return if (value < current.value) {
+            containsNodeRecursive(current.left, value)
+        } else {
+            containsNodeRecursive(current.right, value)
+        }
+    }
+
+    fun deleteNode(value: T) {
+        root = deleteNodeRecursive(root, value)
+    }
+
+    private fun deleteNodeRecursive(current: Node<T>?, value: T): Node<T>? {
+        if (current == null) {
+            return null
+        }
+
+        if (value == current.value) {
+            // Node to delete found
+            if (current.left == null && current.right == null) {
+                return null
+            }
+
+            if (current.right == null) {
+                return current.left
+            }
+
+            if (current.left == null) {
+                return current.right
+            }
+
+            val smallestValue = findSmallestValue(current.right!!)
+            current.value = smallestValue.value
+            current.right = deleteNodeRecursive(current.right, smallestValue.value)
+            return current
+        }
+
+        if (value < current.value) {
+            current.left = deleteNodeRecursive(current.left, value)
+            return current
+        }
+
+        current.right = deleteNodeRecursive(current.right, value)
+        return current
+    }
+
+    private fun findSmallestValue(root: Node<T>): Node<T> {
+        return if (root.left == null) {
+            root
+        } else {
+            findSmallestValue(root.left!!)
+        }
+    }
+
+    //get root node of binary tree as Node.value
+    fun getRootNode(): String {
+        return root!!.value.toString()
+    }
+
+
+    //get random Node.value from binary tree
+    fun getRandomNode(randomInt: Int): String {
+        var randomNode: Node<T>? = null
+        var counter = 0
+        val random = Random()
+        val randomInt = random.nextInt(randomInt)
+        val iterator = object : Iterator<Node<T>> {
+            private var stack = Stack<Node<T>>()
+
+            init {
+                pushLeft(root)
+            }
+
+            private fun pushLeft(node: Node<T>?) {
+                var node = node
+                while (node != null) {
+                    stack.push(node)
+                    node = node.left
+                }
+            }
+
+            override fun hasNext(): Boolean {
+                return !stack.isEmpty()
+            }
+
+            override fun next(): Node<T> {
+                val node = stack.pop()
+                pushLeft(node.right)
+                return node
+            }
+        }
+        while (iterator.hasNext()) {
+            randomNode = iterator.next()
+            if (counter == randomInt) {
+                break
+            }
+            counter++
+        }
+        return randomNode!!.value.toString() + " " + counter
+    }
+
+    //get Node.value from binary Tree for given index
+    fun getNodeByIndex(index: Int): String {
+        var randomNode: Node<T>? = null
+        var counter = 0
+        val iterator = object : Iterator<Node<T>> {
+            private var stack = Stack<Node<T>>()
+
+            init {
+                pushLeft(root)
+            }
+
+            private fun pushLeft(node: Node<T>?) {
+                var node = node
+                while (node != null) {
+                    stack.push(node)
+                    node = node.left
+                }
+            }
+
+            override fun hasNext(): Boolean {
+                return !stack.isEmpty()
+            }
+
+            override fun next(): Node<T> {
+                val node = stack.pop()
+                pushLeft(node.right)
+                return node
+            }
+        }
+        while (iterator.hasNext()) {
+            randomNode = iterator.next()
+            if (counter == index) {
+                break
+            }
+            counter++
+        }
+        return randomNode!!.value.toString()
+    }
+
+    //search for Node.value in binary tree when arg is String if found return true else false
+    fun searchNode(value: String): Boolean {
+        var randomNode: Node<T>? = null
+        var counter = 0
+        val iterator = object : Iterator<Node<T>> {
+            private var stack = Stack<Node<T>>()
+
+            init {
+                pushLeft(root)
+            }
+
+            private fun pushLeft(node: Node<T>?) {
+                var node = node
+                while (node != null) {
+                    stack.push(node)
+                    node = node.left
+                }
+            }
+
+            override fun hasNext(): Boolean {
+                return !stack.isEmpty()
+            }
+
+            override fun next(): Node<T> {
+                val node = stack.pop()
+                pushLeft(node.right)
+                return node
+            }
+        }
+        while (iterator.hasNext()) {
+            randomNode = iterator.next()
+            if (randomNode.value.toString() == value) {
+                return true
+            }
+            counter++
+        }
+        return false
+    }
+
+    //setup binary tree to null
+    fun clear() {
+        root = null
+    }
+
+}
--- app/src/main/java/com/asimin/speedofbinarytree/MainActivity.kt
+++ app/src/main/java/com/asimin/speedofbinarytree/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.asimin.speedofbinarytree
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.view.View.inflate
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
@@ -11,6 +9,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,21 +25,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btGenerateArray: Button
     private lateinit var btGenerateBT: Button
     private lateinit var btSearch: Button
-    private lateinit var view: View
+//    private lateinit var view: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        view = inflate(this, R.layout.activity_main, null)
         numberOfItems = findViewById<EditText>(R.id.etNumberOfItems)
         search = findViewById<EditText>(R.id.etSearch)
         btGenerateArray = findViewById<Button>(R.id.btnGenerateArray)
         btGenerateBT = findViewById<Button>(R.id.btnGenerateBT)
         btSearch = findViewById<Button>(R.id.btnSearch)
         val tvArrayCreationTime = findViewById<TextView>(R.id.tvArrayCreationTime)
+        val tvArraySearchTime = findViewById<TextView>(R.id.tvArraySearchTime)
+        val tvBTSearchTime = findViewById<TextView>(R.id.tvBinaryTreeSearchTime)
         val tvBtCreationTime = findViewById<TextView>(R.id.tvBinaryTreeCreationTime)
         var numberOfItemsValidator = false
+        val binaryTree = BinaryTree<String>()
 
         numberOfItems.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
@@ -46,15 +50,9 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Please enter a number OFCL", Toast.LENGTH_SHORT).show()
                 } else {
                     numberOfItemsValidator = true
-                    Toast.makeText(
-                        this,
-                        "LOST FOCUS - Number of items: ${numberOfItems.text}",
-                        Toast.LENGTH_SHORT
-                    ).show()
                 }
             } else {
                 search.clearFocus()
-                Toast.makeText(this, "GOT FOCUS - clear Search Focus", Toast.LENGTH_SHORT).show()
             }
         }
         numberOfItems.doOnTextChanged { text, start, before, count ->
@@ -62,7 +60,6 @@ class MainActivity : AppCompatActivity() {
                 numberOfItemsValidator = false
                 tvArrayCreationTime.visibility = View.INVISIBLE
                 tvBtCreationTime.visibility = View.INVISIBLE
-                Toast.makeText(this, "Please enter a number OTC", Toast.LENGTH_SHORT).show()
             } else {
                 numberOfItemsValidator = true
             }
@@ -73,17 +70,14 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter a number Array-OCL", Toast.LENGTH_SHORT).show()
             } else {
                 hideKeyboard()
-                Toast.makeText(this, "Generate Array", Toast.LENGTH_SHORT).show()
                 tvArrayCreationTime.visibility = View.VISIBLE
             }
         }
         btGenerateBT.setOnClickListener {
-            if (!numberOfItemsValidator) {
-                Toast.makeText(this, "Please enter a number BT-OCL", Toast.LENGTH_SHORT).show()
-            } else {
+            if (numberOfItemsValidator) {
                 hideKeyboard()
-                Toast.makeText(this, "Generate BT", Toast.LENGTH_SHORT).show()
                 tvBtCreationTime.visibility = View.VISIBLE
+                generateBinaryTree(binaryTree, tvBtCreationTime)
             }
         }
 
@@ -98,14 +92,40 @@ class MainActivity : AppCompatActivity() {
                 numberOfItemsValidator = false
                 search.clearFocus()
                 numberOfItems.requestFocus()
-                Toast.makeText(this, "Please enter a number in SEARCH", Toast.LENGTH_SHORT).show()
             } else {
                 hideKeyboard()
-                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+                //create string searchPrefix which will be created from the search text as text until fist space
+                //if there is no space, then searchPrefix will be the same as search text
+                val searchPrefix = search.text.toString().substringBefore(" ")
+                val startTime = System.currentTimeMillis()
+                search.setText(searchPrefix + " " + binaryTree.searchNode(searchPrefix))
+                val endTime = System.currentTimeMillis()
+                val timeTaken = endTime - startTime
+                tvBTSearchTime.visibility = View.VISIBLE
+                "Search Time: $timeTaken ms".also { tvBTSearchTime.text = it }
             }
         }
 
     }
+
+    private fun generateBinaryTree(binaryTree: BinaryTree<String>, tvBtCreationTime: TextView) {
+        lifecycleScope.launch {
+            binaryTree.clear()
+            val startTime = System.currentTimeMillis()
+            withContext(Dispatchers.Default) {
+                for (i in 1..numberOfItems.text.toString().toInt()) {
+                    binaryTree.addNode(generateRandomString())
+                }
+            }
+            val endTime = System.currentTimeMillis()
+            val timeTaken = endTime - startTime
+            "Generated in: $timeTaken ms".also { tvBtCreationTime.text = it }
+            search.setText(binaryTree.getRandomNode(numberOfItems.text.toString().toInt()-1))
+        }
+        Toast.makeText(this, "Binary Tree creation in progress...", Toast.LENGTH_SHORT).show()
+    }
+
+
     private fun hideKeyboard() {
         val view = this.currentFocus
         if (view != null) {
@@ -113,6 +133,14 @@ class MainActivity : AppCompatActivity() {
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
     }
+    private fun generateRandomString(): String {
+        val charPool: List<Char> = ('A'..'Z') + ('a'..'z')
+        return (1..10).map { kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+    }
+
+
 
 }
 
